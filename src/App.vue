73a696cee02b7e5a0f61911fa295ecf7e4185fe3
Ednildo <ednildocunha@gmail.com>
2022-06-01 16:59:41 -0300

    condicional para lista de tarefas vazia

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,9 @@
       <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
-
+        <Box v-if="listaEstaVazia">
+          Você não está muito produtivo hoje :(
+        </Box>
       </div>
     </div>
 
@@ -17,6 +19,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
+import Box from './components/Box.vue';
 import Formulario from './components/Formulario.vue';
 import Tarefa from './components/Tarefa.vue';
 import ITarefa from './interfaces/ITarefa';
@@ -26,11 +29,17 @@ export default defineComponent({
   components: {
     BarraLateral,
     Formulario,
-    Tarefa
+    Tarefa,
+    Box
   },
   data() {
     return {
       tarefas: [] as ITarefa[]
+    }
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0
     }
   },
   methods: {
--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="box has-text-weight-bold">
+    <Box>
         <div class="columns">
             <div class="column is-7">
-                {{ tarefa.descricao }}
+                {{ tarefa.descricao || 'Tarefa sem descrição' }}
             </div>
             <div class="column">
                 <Cronometro :tempoSegundos="tarefa.duracaoSegundos" />
             </div>
         </div>
-    </div>
+    </Box>
 </template>
 
 <script lang="ts">
 import ITarefa from '@/interfaces/ITarefa'
 import { defineComponent, PropType } from 'vue'
+import Box from './Box.vue'
 import Cronometro from './Cronometro.vue'
 
 export default defineComponent({
     name: 'Tarefa-Nome',
     components: {
         Cronometro,
+        Box
     },
     props: {
         tarefa: {
@@ -30,8 +32,3 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-.box {
-    background: #FAF0CA;
-}
-</style>
